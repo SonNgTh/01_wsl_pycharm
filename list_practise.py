f849ea7fe058dd37e9bcd554466463a4ffa31431
SonNgTh <Sonnt.emc@gmail.com>
2025-04-13 07:14:49 +0700

    updated

--- list_practise.py
+++ list_practise.py
@@ -5,7 +5,7 @@ Ghép 2 dict, mỗi item bằng tổng value của 2 item cùng key trong các d
 dict1 = {'a': 100, 'b': 200, 'c': 300}
 dict2 = {'a': 300, 'b': 200, 'd': 400}
 
- dict3 = {'a': 400, 'b': 400, 'c': 300, 'd': 400}
+# dict3 = {'a': 400, 'b': 400, 'c': 300, 'd': 400}
 dict3 = dict1 | dict2
 
 print(dict3)
@@ -18,7 +18,5 @@ for item in dict3:
             dict3[item] = dict1[item]
         except:
             dict3[item] = dict2[item]
-
-
 
 print(dict3)
