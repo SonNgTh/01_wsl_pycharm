da1db7f3c5f156e0d4fd24c9253abe4ec79a6922
SonNgTh <Sonnt.emc@gmail.com>
2025-04-13 07:14:02 +0700

    updated

---
+++ list_practise.py
@@ -0,0 +1,24 @@
+"""
+Ghép 2 dict, mỗi item bằng tổng value của 2 item cùng key trong các dict còn lại.
+"""
+
+dict1 = {'a': 100, 'b': 200, 'c': 300}
+dict2 = {'a': 300, 'b': 200, 'd': 400}
+
+ dict3 = {'a': 400, 'b': 400, 'c': 300, 'd': 400}
+dict3 = dict1 | dict2
+
+print(dict3)
+
+for item in dict3:
+    try: 
+        dict3[item] = dict1[item] + dict2[item]
+    except:
+        try:
+            dict3[item] = dict1[item]
+        except:
+            dict3[item] = dict2[item]
+
+
+
+print(dict3)
